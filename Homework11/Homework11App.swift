10caefaf1b844ab88404d3c54dcece65ad170195
Andrew Yee <ios@hashbrownlabs.com>
2025-11-04 06:23:49 -0800

    Initial Commit

---
+++ Homework11/Homework11App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework11App.swift
+//  Homework11
+//
+//  Created by Andrew Yee on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
